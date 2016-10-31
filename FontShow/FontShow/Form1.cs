f551e48a63728f9c0e890e29c35a7655cfd44936
rinkako <jiajidi@163.com>
2016-10-31 10:30:05 +0800

    fontshow

---
+++ FontShow/FontShow/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FontShow
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			FontDialog fontDialog = new FontDialog();
+			if (fontDialog.ShowDialog() != DialogResult.Cancel)
+			{
+				this.textBox1.Font = fontDialog.Font;
+			}
+		}
+	}
+}
